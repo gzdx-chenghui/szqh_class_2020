180603ecbfbc392a5333d8c31539dced6421b083
blackgolfer <zhifengzhang60@hotmail.com>
2020-03-09 11:18:01 +0800

    remove _build, updated conf.py, preparing github pages

--- conf.py
+++ conf.py
@@ -14,13 +14,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import sphinxcontrib
-#import sphinx_rtd_theme
-#import alabaster
-#import guzzle_sphinx_theme
+
 # -- Project information -----------------------------------------------------
 
-project = '项目概况'
+project = '量化投资学习项目'
 copyright = '2020, 张志峰'
 author = '张志峰'
 
@@ -33,15 +30,15 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.tikz'
-             ,'sphinx_rtd_theme'
-             ,'recommonmark'
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.tikz',
+    'recommonmark',
+    'sphinx_rtd_theme'
 ]
-
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
-}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -56,34 +53,10 @@ language = 'zh_CN'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-#html_theme_path = guzzle_sphinx_theme.html_theme_path()
-#html_theme = 'guzzle_sphinx_theme'
-#html_theme = 'agogo'
-html_theme_options = {
-    "style_external_links": "true",
-    #"style_nav_header_background": "#0b750a"
-}
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_css_files = {
-    'css/custom.css',
-}
-
-#html_style = 'css/custum.css'
-exclude_patterns =  ['docs/projects/mfm/mfm.rst'
+exclude_patterns =  ['_build'
+                    ,'Thumbs.db'
+                    ,'.DS_Store'
+                    ,'docs/projects/mfm/mfm.rst'
                     ,'docs/projects/abs/abs.rst'
                     ,'docs/projects/mpp/mpp.rst'
                     ,'docs/projects/os/os.rst'
@@ -92,3 +65,27 @@ exclude_patterns =  ['docs/projects/mfm/mfm.rst'
                     ,'docs/typesetting/markdown.rst'
                     ,'docs/typesetting/sphinx.rst'
                     ]
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_theme_options = {
+    "style_external_links": "true",
+    #"style_nav_header_background": "#0b750a"
+}
+
+html_css_files = {
+    'css/custom.css',
+}
+
+# -- Extension configuration -------------------------------------------------
--- index.rst
+++ index.rst
@@ -1,10 +1,10 @@
-.. 项目概况 documentation master file, created by
-   sphinx-quickstart on Thu Mar  5 18:36:01 2020.
+.. 量化投资学习项目 documentation master file, created by
+   sphinx-quickstart on Mon Mar  9 10:30:13 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-项目概况
-====================================
+Welcome to 量化投资学习项目's documentation!
+============================================
 
 .. toctree::
    :maxdepth: 2
